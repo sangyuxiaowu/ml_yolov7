ed92c26b2218cdfd511e21e37a06282a105bc16a
sangsq <sang93@qq.com>
2022-11-05 16:37:25 +0800

    测试通过

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ml_yolov7
+﻿# ml_yolov7
+
+ML.NET Yolov7
+
+模型文件 [yolov7.onnx](https://download.csdn.net/download/marin1993/86912472) 
+
+来自项目 https://github.com/WongKinYiu/yolov7 的 `yolov7.pt` 导出，原项目 releases v0.1 提供的无法使用，需自行调整参数导出。
+
+```bash
+python .\export.py --weights=yolov7.pt --grid --simplify
+```
--- ml_yolov7/Program.cs
+++ ml_yolov7/Program.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.ML;
+﻿using System.Drawing;
+using Yolov5Net.Scorer;
+using Yolov5Net.Scorer.Models;
 
 internal class Program
 {
+    static readonly string assetsPath = GetAbsolutePath(@"../../../assets");
+    static readonly string modelFilePath = Path.Combine(assetsPath, "Model", "yolov7.onnx");
+    static readonly string imagesFolder = Path.Combine(assetsPath, "images");
+    static readonly string outputFolder = Path.Combine(assetsPath, "images", "output");
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (!Directory.Exists(outputFolder))
+        {
+            Directory.CreateDirectory(outputFolder);
+        }
+        var imgs = Directory.GetFiles(imagesFolder).Where(filePath => Path.GetExtension(filePath) == ".jpg");
 
+        using var scorer = new YoloScorer<YoloCocoP5Model>(modelFilePath);
+
+        foreach (var imgsFile in imgs)
+        {
+            using var image = Image.FromFile(imgsFile);
+            List<YoloPrediction> predictions = scorer.Predict(image);
+            using var graphics = Graphics.FromImage(image);
+            foreach (var prediction in predictions)
+            {
+                double score = Math.Round(prediction.Score, 2);
+
+                graphics.DrawRectangles(new Pen(prediction.Label.Color, 1),
+                    new[] { prediction.Rectangle });
+
+                var (x, y) = (prediction.Rectangle.X - 3, prediction.Rectangle.Y - 23);
+
+                graphics.DrawString($"{prediction.Label.Name} ({score})",
+                    new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(prediction.Label.Color),
+                    new PointF(x, y));
+            }
+
+            image.Save(Path.Combine(outputFolder, $"result{DateTime.Now.Ticks}.jpg"));
+        }
     }
 
     /// <summary>
